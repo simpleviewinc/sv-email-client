6b86c07468df5f93b2f93362b8b9d24ceb862883
Owen Allen <owenallenaz@gmail.com>
2019-10-09 16:22:06 -0700

    Adds acct_id to send, setup, test_create_data, test_reset_data.

--- src/prefixes/EmailPrefix.js
+++ src/prefixes/EmailPrefix.js
@@ -1,4 +1,4 @@
-const { query } = require("@simpleview/sv-graphql-client");
+const { query, nullToUndefined } = require("@simpleview/sv-graphql-client");
 
 class EmailPrefix {
 	constructor({ graphUrl, graphServer }) {
@@ -11,13 +11,14 @@ class EmailPrefix {
 		context = context || this._graphServer.context;
 
 		const variables = {
-			input
+			input,
+			acct_id : context.acct_id
 		}
 
 		const response = await query({
 			query : `
-				mutation($input: email_send_input!) {
-					email {
+				mutation($acct_id: String!, $input: email_send_input!) {
+					email(acct_id: $acct_id) {
 						send(input: $input) {
 							${fields}
 						}
@@ -30,6 +31,92 @@ class EmailPrefix {
 		});
 		
 		return response.email.send;
+	}
+	async setup({ context, fields }) {
+		context = context || this._graphServer.context;
+		
+		const variables = {
+			acct_id : context.acct_id
+		}
+		
+		const result = await query({
+			query : `
+				mutation($acct_id: String!) {
+					email(acct_id: $acct_id) {
+						setup {
+							${fields}
+						}
+					}
+				}
+			`,
+			variables,
+			url : this._graphUrl,
+			token : context.token
+		});
+		
+		const returnData = result.email.setup;
+		
+		nullToUndefined(returnData);
+		
+		return returnData;
+	}
+	async test_reset_data({ context, fields }) {
+		context = context || this._graphServer.context;
+		
+		console.log("context", context);
+		
+		const variables = {
+			acct_id : context.acct_id
+		}
+		
+		const result = await query({
+			query : `
+				mutation($acct_id: String!) {
+					email(acct_id: $acct_id) {
+						test_reset_data {
+							${fields}
+						}
+					}
+				}
+			`,
+			variables,
+			url : this._graphUrl,
+			token : context.token
+		});
+		
+		const returnData = result.email.test_reset_data;
+		
+		nullToUndefined(returnData);
+		
+		return returnData;
+	}
+	async test_clear_data({ context, fields }) {
+		context = context || this._graphServer.context;
+		
+		const variables = {
+			acct_id : context.acct_id
+		}
+		
+		const result = await query({
+			query : `
+				mutation($acct_id: String!) {
+					email(acct_id: $acct_id) {
+						test_clear_data {
+							${fields}
+						}
+					}
+				}
+			`,
+			variables,
+			url : this._graphUrl,
+			token : context.token
+		});
+		
+		const returnData = result.email.test_clear_data;
+		
+		nullToUndefined(returnData);
+		
+		return returnData;
 	}
 }
 
